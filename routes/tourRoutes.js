83f5f37e7a0db513b890f96d7c3de864c3e136a5
Jingwei Ye <yjjw97@gmail.com>
2020-07-28 20:51:52 -0700

    Aggregation Pipeline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,6 +10,10 @@ const router = express.Router();
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 
+router.route('/tours-stats').get(tourController.getTourStats);
+
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
 router
   .route('/')
   .get(tourController.getAllTours)

b59d4f71df5d1da4d56707999b8424a6d84488ad
Jingwei Ye <yjjw97@gmail.com>
2020-07-29 19:11:58 -0700

    Mongoose Middleware

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ console.log(process.env.NODE_ENV);
 app.use(morgan('dev'));
 app.use(express.json()); //get access to request body of the request object
 app.use(express.static('./public')); //server static files
+/*
 app.use((req, res, next) => {
   console.log('Hello from the middleware!');
   next();
 });
-
+*/
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const slugify = require('slugify');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -7,6 +7,7 @@ const tourSchema = new mongoose.Schema({
     unique: true,
     trim: true
   },
+  slug: String,
   duration: {
     type: Number,
     required:[true, 'A tour must have a duration']
@@ -51,7 +52,53 @@ const tourSchema = new mongoose.Schema({
     default: Date.now(),
     select: false
   },
-  startDates: [Date]
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  toJSON: { virtuals: true} // make virtual fields part of the output
+});
+
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7;
+})
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, {lower: true});
+  next();
+});
+/*
+tourSchema.pre('save', function(next) {
+  console.log('Will save documents...');
+  next();
+})
+
+tourSchema.post('save', function(doc, next) {
+  console.log(doc);
+  next();
+});
+*/
+// QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: {$ne: true} });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({$match: { secretTour: { $ne: true} }});
+  console.log(this.pipeline());
+  next();
 });
 const Tour = mongoose.model('Tour', tourSchema);
 

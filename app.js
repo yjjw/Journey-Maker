e176ac33dcab7963c090fde5f93822e23f0a718e
Jingwei Ye <yjjw97@gmail.com>
2020-07-31 19:42:19 -0700

    Handling Unhadled Routes

--- app.js
+++ app.js
@@ -25,5 +25,14 @@ app.use((req, res, next) => {
 app.use('/api/v2/tours', tourRouter);
 app.use('/api/v2/users', userRouter);
 
+// for all other invalids routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  });
+  next();
+});
+
 module.exports = app;
 

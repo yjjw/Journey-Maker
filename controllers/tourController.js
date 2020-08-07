f5da2aedd60b9647bf9d2a5e849cb729ea975f36
Jingwei Ye <yjjw97@gmail.com>
2020-08-07 16:44:57 -0700

    Add 404 Not Found Error

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,8 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = '-ratingAverage,price';
@@ -32,7 +34,9 @@ exports.getAllTours = catchAsync(async (req,res, next) => {
 
 exports.getTour = catchAsync(async (req,res, next) => {
     const tour = await Tour.findById(req.params.id);
-
+    if (!tour) {
+      return next(new AppError('No tour found with the given ID', 404));
+    }
     res.status(200).json({
       status: 'success',
       data: {
